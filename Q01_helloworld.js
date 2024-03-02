0cebea42534bcd23fe7425cda7714e20d009d02a
Hunain Alam <147208461+Hunainalam01@users.noreply.github.com>
2024-03-02 03:45:41 -0800

    Update Q01_helloworld.js

--- Q01_helloworld.js
+++ Q01_helloworld.js
@@ -1,29 +1,3 @@
-// // ASSIGNMENT OF 45 QUSTIONS:-
-// // Question no 01:-
+// Question no 01:-
+
 // console.log('Hello world!');
-// // Question no 02:-
-// let personName: string = "Eric";
-// console.log(`Hello ${personName}, would you like to learn some Python tooday?`);
-// // Question no 03:-
-// let personName: string = "Hunain alam";
-//for Uppercase
-// console.log(`Uppercase: ${personName.toUpperCase()}`); 
-//for Lowercase
-// console.log(`Lowercase: ${personName.toLowerCase()}`);
-// // for Titlecase
-// console.log("Titlecase:", personName.replace(/\b\w/g, char => char.toUpperCase()));
-// // Question no 04:-
-// let qouteFrom: string = "Charlie Munger";
-// console.log(`${qouteFrom} once said, "The world isn't crazy enough place to reward a whole bunch of Undeserving people" `);
-// // Question no 05:-
-// let famous_person: string = "Ecomjet";
-// let message: string = "Nature test you to see how badly do you want, what you say you want";
-// console.log(`${famous_person} once said, "${message}"`);
-// // Question no 06:-
-var nameWithWhitespace = "\t\n   John Doe   \t\n";
-// console.log("Name with whitespace:");
-// console.log(nameWithWhitespace);
-// Stripping whitespace
-var strippedName = nameWithWhitespace.trim();
-console.log("\nName after stripping whitespace:");
-console.log(strippedName);
