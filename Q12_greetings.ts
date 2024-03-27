a168bb15e1a940e7fc4ddd45f307f0782eb64b8b
Hunain Alam <147208461+Hunainalam01@users.noreply.github.com>
2024-03-26 21:08:57 -0700

    Update Q12_greetings.ts

--- Q12_greetings.ts
+++ Q12_greetings.ts
@@ -1,7 +1,3 @@
-// Greetings: Start with the array you used in Exercise 11, but instead of
-//  just printing each person’s name, print a message to them. The text of each
-//   message should be the same, but each message should be personalized with the person’s name.
-
 // Question no 12:-
 
 let names:string[] = ["Misbah", "Ammar", "Wazeer", "Saba", "Kawish"];
